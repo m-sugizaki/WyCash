bed0b1823583e41d2c62ab4d6a6057a2f9caa54b
Mutsumi Sugizaki <m-sugizaki@cubesystem.co.jp>
2019-11-01 17:32:43 +0900

    9-3

--- src/money/Franc.java
+++ src/money/Franc.java
@@ -1,14 +1,10 @@
 package money;
 
 class Franc  extends Money{
-	private String currency;
 	
 	Franc(int amount) {
 		this.amount = amount ;
 		currency = "CHF";
-	}
-	String currency() {
-		return currency;
 	}
 	Money times(int multiplier) {
 		return new Franc(amount * multiplier);
