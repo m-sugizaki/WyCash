c3930380e33500c48185487495ccff1e2d705700
Mutsumi Sugizaki <m-sugizaki@cubesystem.co.jp>
2019-11-01 17:41:07 +0900

    9-6

--- src/money/Money.java
+++ src/money/Money.java
@@ -16,7 +16,7 @@ abstract class Money {
 	}
 
 	static Money dollar(int amount) {
-		return new Dollar(amount);
+		return new Dollar(amount, "USD");
 	}
 	static Money franc(int amount) {
 		return new Franc(amount, "CHF");
