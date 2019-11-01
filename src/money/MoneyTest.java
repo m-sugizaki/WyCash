56fecdfc2d43e8fd5e78f66e1795b22453ccd54e
Mutsumi Sugizaki <m-sugizaki@cubesystem.co.jp>
2019-11-01 17:26:55 +0900

    9-1

--- src/money/MoneyTest.java
+++ src/money/MoneyTest.java
@@ -19,6 +19,11 @@ public class MoneyTest {
 
 		assertFalse(Money.franc(5).equals(Money.dollar(5)));
 	}
+	@Test
+	public void testCurrency() {
+		assertEquals("USD", Money.dollar(1).currency());
+		assertEquals("CHF", Money.franc(1).currency());
+	}
 
 	@Test
 	public void testFrancMultiplication() {
