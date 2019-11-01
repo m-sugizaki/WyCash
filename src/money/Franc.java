094faee59439090b5a44f96ff5b97202b0bfb09c
Mutsumi Sugizaki <m-sugizaki@cubesystem.co.jp>
2019-11-01 17:58:23 +0900

    10-5

--- src/money/Franc.java
+++ src/money/Franc.java
@@ -6,7 +6,7 @@ class Franc  extends Money{
 		super(amount, currency);
 	}
 	Money times(int multiplier) {
-		return new Money(amount * multiplier, currency);
+		return new Franc(amount * multiplier, currency);
 //		return Money.franc(amount * multiplier);
 	}
 }
