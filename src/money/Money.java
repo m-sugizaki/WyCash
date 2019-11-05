8e9a40ba0fa1b841eb984d3ee4433e2407d71f8f
Mutsumi Sugizaki <m-sugizaki@cubesystem.co.jp>
2019-11-05 09:53:18 +0900

    12-1

--- src/money/Money.java
+++ src/money/Money.java
@@ -11,6 +11,11 @@ class Money {
 	Money times(int multiplier) {
 		return new Money(amount * multiplier, currency);
 	}
+	Money plus(Money addend) {
+		return new Money(amount + addend.amount, currency);
+	}
+	
+	
 	String currency() {
 		return currency;
 	}
